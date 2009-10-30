ea64380e33a3a1001da5f2401f3fe6e74972faac
unknown <sam@.(none)>
2009-10-30 20:02:03 +1100

    .

--- lib/build-libb.sh
+++ lib/build-libb.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 v() {
 	echo "$@" >&2
 	"$@"
@@ -8,6 +8,7 @@ LIBB_BUILD=1
 . brace_env
 modules="str types buffer circbuf deq thunk list hash error process vec cstr rope sym cons path time m env find ccomplex proc priq timeout net vio io alloc util selector sched shuttle sock ccoro main darcs html http hunk cgi qmath mime meta sound music colours sprite gr event turtle tsv key b"
 time (
+	time brace_update_headers
 	time (
 		files=""
 		for A in $modules; do
@@ -16,9 +17,10 @@ time (
 				if [ -e "$f" ]; then files="$files $f"; fi
 			done
 		done
-		( echo "use png.h" ; v cat $files | grep -v '^\(use\|export\) [^\.]*$' ) > all.b
+		( echo "use png.h" ; v cat $files | grep -v '^\(use\|export\) [^\.]*$' ) > .all.b
 	)
-	time v b2c < all.b > all.c
-	time v $CC -o $SONAME $CFLAGS -shared -fpic -Wl,-soname,$SONAME $LDFLAGS all.c $LDLIBS 2>&1 | tee libb.log
+	time v b2c < .all.b >.all.c
+	time v $CC -o $SONAME $CFLAGS -shared -fpic -Wl,-soname,$SONAME $LDFLAGS .all.c $LDLIBS 2>&1 | tee libb.log
 	chmod -x $SONAME
+	rm .all.b all.c
 )
