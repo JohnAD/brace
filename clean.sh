a93dd4660c0c6f405f49ccb42580c79c88485d6a
Sam Watkins <sam@nikas.(none)>
2009-11-28 22:03:08 +1100

    .

--- clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd "`dirname "$0"`"
-make distclean
-exit 0
-#fakeroot debian/rules clean
--- rh-setup.sh
+++
@@ -1,2 +0,0 @@
-yum install libX11-devel libXext-devel perl-IO-String libpng-devel ncurses-devel 
-ln -s /usr/lib/perl5/site_perl /usr/local/lib/site_perl
